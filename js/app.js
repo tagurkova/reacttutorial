fd2c3507c8b6667563b10434a68fd4c58ef3221b
tagurkova <tatsiana.agurkova@artezio.com>
2017-09-25 18:10:11 +0300

    First components

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
+let Comments = React.createClass({
+   render: function() {
+       return (
+           <div className="comments">
+              Нет новостей - комментировать нечего.
+           </div>
+        );
+   }
+});
+
+let News = React.createClass({
+    render: function() {
+        return (
+            <div className="news">
+                К сожалению, новостей нет.
+            </div>
+        );
+    }
+});
+
+let App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                Всем привет, я компонент App! Я умею отображать новости.
+                <News />
+                <Comments />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <App />,
     document.getElementById('root')
 );
