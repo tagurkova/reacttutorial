69ae24d14f83c5df9692fa6b609393d8d421b5b2
tagurkova <tatsiana.agurkova@artezio.com>
2017-09-26 09:31:44 +0300

    Component props

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+let my_news = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четверг, четвертого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
+
 let Comments = React.createClass({
    render: function() {
        return (
@@ -10,9 +25,21 @@ let Comments = React.createClass({
 
 let News = React.createClass({
     render: function() {
+        let data = this.props.data;
+        let newsTemplate = data.map(function(item, index) {
+            return (
+                <div key={index}>
+                    <p className="news__author">{item.author}:</p>
+                    <p className="news__text">{item.text}</p>
+                </div>
+            )
+        });
+
+        console.log(newsTemplate);
+
         return (
             <div className="news">
-                К сожалению, новостей нет.
+                {newsTemplate}
             </div>
         );
     }
@@ -23,7 +50,7 @@ let App = React.createClass({
         return (
             <div className="app">
                 Всем привет, я компонент App! Я умею отображать новости.
-                <News />
+                <News data={my_news}/>  {/*добавили свойство data */}
                 <Comments />
             </div>
         );
