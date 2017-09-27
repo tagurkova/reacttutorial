6fc2dfc7c4c6ded9fd311c2776cf74eed181591c
tagurkova <tatyana769@gmail.com>
2017-09-27 14:59:04 +0300

    Clean prev commit

--- js/app.js
+++ js/app.js
@@ -31,7 +31,6 @@ let Article = React.createClass({
             visible: false
         }
     },
-
     readmoreClick: function (e) {
         e.preventDefault();
         this.setState({visible: true});
